9877990add3beed65fac42960ab5296761e1f63c
DeletedUser1836 <den.pylypen@gmail.com>
2025-05-08 13:10:57 +0200

    encoding works now, yupiiiiiiiiiiiiiii

--- something.cpp
+++ something.cpp
@@ -5,7 +5,7 @@ using  namespace std;
 int main()
 {                       /*A     B       C      D    E   F       G    H      I      J    K       L   M      N    O      P      Q     R     S    T    U     V      W     X       Y      Z      0       1        2      3       4       5       6       7        8      9        .       ,         ?*/
     string MorsAlf[39] = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--..","-----",".----","..---","...--","....-",".....","-....","--...","---..","----.",".-.-.-","--..--","..--.."};
-    string NormAlf[39] = {"A","B","C","D","E","F","G","H","I","J","K","L","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","0","1","2","3","4","5","6","7","8","9",".",",","?"};
+    string NormAlf[39] = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","0","1","2","3","4","5","6","7","8","9",".",",","?"};
     string UsrInput = "";
     unsigned int opt = 0;
     while (true)
@@ -32,12 +32,13 @@ int main()
             //encoding
             cout << "Please enter the message you want to encode: "<<endl;
             cin >> UsrInput;
+            cout  << "This is '" << UsrInput << "' in Morse Code: "<<endl;
             for(long long i = 0; i < UsrInput.length(); i++)
             {
-                for(long long j = 0; j < 39; i++)
+                for(long long j = 0; j < 39; j++)
                 {
                     char CrntLetter = NormAlf[j][0];
-                    if(UsrInput[i] == CrntLetter)
+                    if(toupper(UsrInput[i]) == CrntLetter)
                     {
                         cout << MorsAlf[j]<< " ";
                         break;
@@ -49,6 +50,8 @@ int main()
             cout << "Wrong argument provided. Please run the script again.";
             exit(1);
     }
+
+    cout <<endl <<endl <<endl;
 
     return 0;
 }
