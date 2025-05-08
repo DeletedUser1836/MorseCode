724958a0a2920de618289de8c84c560b801139d8
DeletedUser1836 <den.pylypen@gmail.com>
2025-05-08 12:58:47 +0200

    encoding in morse (maybe)

--- something.cpp
+++ something.cpp
@@ -6,14 +6,14 @@ int main()
 {                       /*A     B       C      D    E   F       G    H      I      J    K       L   M      N    O      P      Q     R     S    T    U     V      W     X       Y      Z      0       1        2      3       4       5       6       7        8      9        .       ,         ?*/
     string MorsAlf[39] = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--..","-----",".----","..---","...--","....-",".....","-....","--...","---..","----.",".-.-.-","--..--","..--.."};
     string NormAlf[39] = {"A","B","C","D","E","F","G","H","I","J","K","L","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","0","1","2","3","4","5","6","7","8","9",".",",","?"};
-    string  UsrInput = "";
-    char opt = 0;
-    bool isMorse = false;
+    string UsrInput = "";
+    string ExitMsg = "";
+    unsigned char opt = 0;
     while (true)
     {
         cout << "Do you want to decode or encode?[1-dec/2-enc]";
         cin >> opt;
-        if(opt == 1 || opt == 2)
+        if(opt==1||opt==2)
         {
             break;
         }
@@ -28,17 +28,30 @@ int main()
         case 1:
             cout << "Please enter the message you want to decode: " << endl;
             cin  >> UsrInput;
-            isMorse = true;
             break;
         case 2:
             cout << "Please enter the message you want to encode: "<< endl;
             cin >> UsrInput;
-            isMorse = false;
             break;
         default:
             cout << "Wrong argument provided. Please run the script again.";
             exit(1);
     }
     
+    //encoding
+    for(long long i = 0; i < UsrInput.length(); i++)
+    {
+        for(long long j = 0; j < 39; i++)
+        {
+            char CrntLetter = NormAlf[j][0];
+            if(UsrInput[i] == CrntLetter)
+            {
+                cout << MorsAlf[j]<< " ";
+                break;
+            }
+        }
+    }
+
+
     return 0;
 }
