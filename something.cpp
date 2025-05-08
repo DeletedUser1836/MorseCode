5b74a7bd7f3c53827d7627539536c31184088985
DeletedUser1836 <den.pylypen@gmail.com>
2025-05-08 12:10:18 +0200

    start of the code

---
+++ something.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using  namespace std;
+
+int main()
+{
+    string MorsAlf[26] = {".-","-...","-.-.","-..",".","..-.","--."};
+    string NormAlf[26] = {"","","","",""};
+    return 0;
+}
