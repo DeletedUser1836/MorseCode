7592c972aec682a5251bf7c21e995f13af90eab1
DeletedUser1836 <den.pylypen@gmail.com>
2025-05-08 13:02:34 +0200

    minor tweaks + encoding doesn work

--- something.cpp
+++ something.cpp
@@ -7,8 +7,7 @@ int main()
     string MorsAlf[39] = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--..","-----",".----","..---","...--","....-",".....","-....","--...","---..","----.",".-.-.-","--..--","..--.."};
     string NormAlf[39] = {"A","B","C","D","E","F","G","H","I","J","K","L","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","0","1","2","3","4","5","6","7","8","9",".",",","?"};
     string UsrInput = "";
-    string ExitMsg = "";
-    unsigned char opt = 0;
+    unsigned int opt = 0;
     while (true)
     {
         cout << "Do you want to decode or encode?[1-dec/2-enc]";
@@ -19,39 +18,37 @@ int main()
         }
         else
         {
-            cout<<"Invalid argument, please enter again.";
+            cout<<"Invalid argument, please enter again."<<endl;
         }
     }
     
     switch(opt)
     {
         case 1:
-            cout << "Please enter the message you want to decode: " << endl;
+            cout << "Please enter the message you want to decode: " <<endl;
             cin  >> UsrInput;
             break;
         case 2:
-            cout << "Please enter the message you want to encode: "<< endl;
+            //encoding
+            cout << "Please enter the message you want to encode: "<<endl;
             cin >> UsrInput;
+            for(long long i = 0; i < UsrInput.length(); i++)
+            {
+                for(long long j = 0; j < 39; i++)
+                {
+                    char CrntLetter = NormAlf[j][0];
+                    if(UsrInput[i] == CrntLetter)
+                    {
+                        cout << MorsAlf[j]<< " ";
+                        break;
+                    }
+                }
+            }
             break;
         default:
             cout << "Wrong argument provided. Please run the script again.";
             exit(1);
     }
-    
-    //encoding
-    for(long long i = 0; i < UsrInput.length(); i++)
-    {
-        for(long long j = 0; j < 39; i++)
-        {
-            char CrntLetter = NormAlf[j][0];
-            if(UsrInput[i] == CrntLetter)
-            {
-                cout << MorsAlf[j]<< " ";
-                break;
-            }
-        }
-    }
-
 
     return 0;
 }
